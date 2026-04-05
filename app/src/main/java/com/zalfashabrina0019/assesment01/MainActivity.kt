e18b100aca20762dbfb4ddf1c337d1303c3e217b
Zalfa Shabrina Raya Rezki <zalfashabrina06@gmail.com>
2026-04-05 15:11:47 +0700

    Menambahkan gambar untuk main screen

--- app/src/main/java/com/zalfashabrina0019/assesment01/MainActivity.kt
+++ app/src/main/java/com/zalfashabrina0019/assesment01/MainActivity.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -14,9 +18,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.zalfashabrina0019.assesment01.ui.theme.Assesment01Theme
 
 class MainActivity : ComponentActivity() {
@@ -53,10 +63,20 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android",
-        modifier = modifier
-    )
+    val image = painterResource(R.drawable.marurent)
+
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = image,
+            contentDescription = stringResource(R.string.gambar),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(132.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
