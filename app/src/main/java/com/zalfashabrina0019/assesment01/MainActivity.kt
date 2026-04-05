00314b01a8b4d6e4107d45aa9fb8863a76466083
Zalfa Shabrina Raya Rezki <zalfashabrina06@gmail.com>
2026-04-05 15:32:09 +0700

    Menambahkan Deskripsi Text

--- app/src/main/java/com/zalfashabrina0019/assesment01/MainActivity.kt
+++ app/src/main/java/com/zalfashabrina0019/assesment01/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -25,6 +27,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.zalfashabrina0019.assesment01.ui.theme.Assesment01Theme
@@ -65,11 +69,14 @@ fun MainScreen() {
 fun ScreenContent(modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.marurent)
     val localFont = FontFamily(
-        Font(R.font.poppins_bold)
+        Font(R.font.poppins_bold, FontWeight.Bold),
+        Font(R.font.poppins_medium, FontWeight.Medium)
     )
 
     Column(
-        modifier = modifier.fillMaxSize().padding(16.dp),
+        modifier = modifier.fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -82,10 +89,21 @@ fun ScreenContent(modifier: Modifier = Modifier) {
         Text(
             text = stringResource(R.string.app_name),
             fontFamily = localFont,
+            fontWeight = FontWeight.Bold,
             style = MaterialTheme.typography.headlineLarge,
             color =  MaterialTheme.colorScheme.primary,
             modifier = Modifier.padding(top = 3.dp)
         )
+        Text(
+            text = stringResource(R.string.desc),
+            fontFamily = localFont,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodySmall,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(top = 2.dp)
+        )
+
     }
 }
 
